950863db3196d0dda4a51ecbc0b29b2ed84b025e
andreastkdf <andreaskostakis@gmail.com>
2019-08-06 12:58:33 +0100

    First Reducers

---
+++ reducers/index.js
@@ -0,0 +1,20 @@
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from "../actions/index"
+
+const decks = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_DECKS:
+      return {
+        ...state,
+        ...action.decks
+      }
+    case ADD_DECK:
+      return {
+        ...state,
+        ...action.title
+      }
+    default:
+      return state
+  }
+}
+
+export default decks
