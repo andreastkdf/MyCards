7d2765d4cfd48fe3f395a76f3b282c5da66ef4ad
andreastkdf <andreaskostakis@gmail.com>
2019-08-06 16:57:26 +0100

    Methods to interact with the AsyncStorage

---
+++ utils/api.js
@@ -0,0 +1,26 @@
+import { AsyncStorage } from "react-native"
+import { setDummyData, DECKS_STORAGE_KEY } from "./_decks"
+
+export const getDecks = () => {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => {
+    return results === null ? setDummyData() : JSON.parse(results)
+  })
+}
+
+export const getDeck = id => {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => {
+    const data = JSON.parse(results)
+    return data[id]
+  })
+}
+
+export const saveDeckTitle = ({ title, id }) => {
+  return AsyncStorage.mergeItem(
+    DECKS_STORAGE_KEY,
+    JSON.stringify({
+      [id]: title
+    })
+  )
+}
+
+// TODO: Add call to addCardToDeck
